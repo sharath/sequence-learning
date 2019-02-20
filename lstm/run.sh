aa240173874a26c054cf9006d709eb12fde7bb81
sharath <sharathramkumar@gmail.com>
2019-02-20 01:34:09 -0500

    clean up

---
+++ lstm/run.sh
@@ -0,0 +1,38 @@
+source ../venv/bin/activate
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+sleep 1.1
+python -W ignore lstm.py &
+
+
