2b75c3997a79826509eca615c6658a134e2a81cc
sharath <sharathramkumar@gmail.com>
2019-02-24 12:25:00 -0500

    fixes

--- tdnn/tdnn.py
+++ tdnn/tdnn.py
@@ -29,7 +29,7 @@ def refresh(sequence, target, it):
 
 net = Net()
 criterion = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+optimizer = optim.SGD(net.parameters(), lr=0.01)#, momentum=0.9)
 encoder = Encoder(seed)
 
 history, sequence, target = [], [], []
@@ -38,40 +38,53 @@ refresh(sequence, target, 0)
 
 start_logging()
 print('it,current,target,prediction,correct')
-for it in range(10, 20000):
+for it in range(0, 20000):
     csymbol = sequence.pop(0)
     tsymbol = target.pop(0)
 
     history.append(csymbol)
 
+
     if len(sequence) == 0:
         refresh(sequence, target, it)
 
+    if it < 10:
+        continue
+    tl = [0]
+    if it > 1000:
+        train = history[max(0, len(history) - 3000):]
+        
+        for _ in range(40):
+            running_loss = 0 
+            for i in range(len(train)-10):
+                x = train[i:i+10]
+                y = train[i+10]
 
-    train = history[max(0, len(history) - 500):]
-    for i in range(len(train)-10):
-        x = train[i:i+10]
-        y = train[i+10]
+                x_enc = torch.zeros(10*25)
+                for j, s in enumerate(x):
+                    x_enc[j*25:(j+1)*25] = encoder.encode(s)
+                y_enc = encoder.encode(y)
 
-        x_enc = torch.zeros(10*25)
-        for j, s in enumerate(x):
-            x_enc[j*25:(j+1)*25] = encoder.encode(s)
-        y_enc = encoder.encode(y)
+                optimizer.zero_grad()
+                y_hat = net(x_enc)
+                loss = criterion(y_hat, y_enc)
 
-        optimizer.zero_grad()
-        y_hat = net(x_enc)
-        loss = criterion(y_hat, y_enc)
-
-        loss.backward()
-        optimizer.step()
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+        tl.append(running_loss)
 
     x_enc = torch.zeros(10*25)
-    for i in range(-10, 0, -1):
+    for i in range(-10, 0, 1):
+        j = i + 10
+        print(j)
         x_enc[j*25:(j+1)*25] = encoder.encode(history[it-i])
 
+    print(x_enc)
 
     output = net(x_enc)
+    print(encoder.encodings)
     prediction = encoder.decode(output)
     correct = int(tsymbol == prediction)
     if len(sequence) == 2:
-        print(f'{it},{csymbol},{tsymbol},{prediction},{correct}')
+        print(f'{it},{csymbol},{tsymbol},{prediction},{correct},{sum(tl)/len(tl):.4f}')
