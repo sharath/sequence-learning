d38c4561c8b666e076b53263c2c1186782ea7223
sharath <hi@sharath.pro>
2019-02-24 02:04:18 -0500

    tdnn implementation

---
+++ tdnn/tdnn.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from dataset import dataset_a, dataset_b, Encoder
+from util import start_logging
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(10*25, 200)
+        self.fc2 = nn.Linear(200, 25)
+
+    def forward(self, x):
+        x = F.sigmoid(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+seed = 100
+def refresh(sequence, target, it):
+    dataset = dataset_a if it < 10000 else dataset_b
+    torch.manual_seed(seed+it)
+
+    c = dataset[torch.randint(0, len(dataset), (1, ))]
+    t = c[1:] + [-1] + [-1]
+    sequence.extend(c + [int(encoder.noise(it))])
+    target.extend(t)
+
+net = Net()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+encoder = Encoder(seed)
+
+history, sequence, target = [], [], []
+refresh(sequence, target, 0)
+
+
+start_logging()
+print('it,current,target,prediction,correct')
+for it in range(10, 20000):
+    csymbol = sequence.pop(0)
+    tsymbol = target.pop(0)
+
+    history.append(csymbol)
+
+    if len(sequence) == 0:
+        refresh(sequence, target, it)
+
+
+    train = history[max(0, len(history) - 500):]
+    for i in range(len(train)-10):
+        x = train[i:i+10]
+        y = train[i+10]
+
+        x_enc = torch.zeros(10*25)
+        for j, s in enumerate(x):
+            x_enc[j*25:(j+1)*25] = encoder.encode(s)
+        y_enc = encoder.encode(y)
+
+        optimizer.zero_grad()
+        y_hat = net(x_enc)
+        loss = criterion(y_hat, y_enc)
+
+        loss.backward()
+        optimizer.step()
+
+    x_enc = torch.zeros(10*25)
+    for i in range(-10, 0, -1):
+        x_enc[j*25:(j+1)*25] = encoder.encode(history[it-i])
+
+
+    output = net(x_enc)
+    prediction = encoder.decode(output)
+    correct = int(tsymbol == prediction)
+    if len(sequence) == 2:
+        print(f'{it},{csymbol},{tsymbol},{prediction},{correct}')
