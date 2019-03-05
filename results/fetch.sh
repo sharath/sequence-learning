18121ccbafc748a22751e4c0c617dbe25c8a651a
sharath <hi@sharath.pro>
2019-03-05 09:48:06 -0500

    🤦

--- results/fetch.sh
+++ results/fetch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-scp sharathramku@gypsum:/home/sharathramku/bigdrive/sharath/sequence-learning/out.zip .
+scp sharathramku@gypsum:/home/sharathramku/bigdrive/sharath/sequence-learning/experiments/out.zip .
 unzip -o out.zip
 rm out.zip
